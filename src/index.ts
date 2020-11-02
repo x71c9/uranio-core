fabf232a94b7d598b1fa6fd935cd34eee475dd3c
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-11-02 21:07:25 +0100

    removed with_sensitive, started implementing validate query paramaters in dal

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,43 @@
 	
 // }
 
-import create_instance from './rsrc/user';
-const b = {
-	_id:'sdjhaskhdjkas',
-	email:'a@a.com',
-	username:'usernmae',
-	malicious:'HDNSKJ(")/)',
-	first_name:'',
-	last_name:'',
-	active:true,
-	password:'',
-	bio:'',
-	type:''
-};
-const a = create_instance(b);
-// a.vali();
-console.log(a);
+// import create_instance from './rsrc/user';
+// const b = {
+//   _id:'sdjhaskhdjkas',
+//   email:'a@a.com',
+//   username:'usernmae',
+//   malicious:'HDNSKJ(")/)',
+//   first_name:'',
+//   last_name:'',
+//   active:true,
+//   password:'',
+//   bio:'',
+//   type:''
+// };
+// const a = create_instance(b);
+// // a.vali();
+// console.log(a);
 
 
+// interface inter {
+	
+//   a:string
+//   b:number
+	
+// }
+
+// class A implements inter{
+//   private c:string;
+//   constructor(public a:string, public b:number){
+//     this.c = this.a + this.b;
+//   }
+//   public console(){
+//     console.log(this.c);
+//   }
+// }
+
+// console.log(Object.keys(new A()));
+
+// for(const k in Object.entries(A)){
+//   console.log(k);
+// }
