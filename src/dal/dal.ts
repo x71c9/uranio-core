39f955f8a7f38f9dc3edd84c84f386fc6e975351
Andrea Reni <mail@nbl7.net>
2020-11-02 21:07:25 +0100

    removed with_sensitive, started implementing validate query paramaters in dal

--- src/dal/dal.ts
+++ src/dal/dal.ts
@@ -4,7 +4,7 @@
  * @packageDocumentation
  */
 
-import {urn_log} from 'urn-lib';
+import {urn_log, urn_util, urn_error} from 'urn-lib';
 
 import * as urn_rsrc from '../rsrc/';
 
@@ -49,90 +49,133 @@ export abstract class URNDAL<Resource extends urn_rsrc.ResourceInstance> {
 	 * @param with_sensitve
 	 *   if set to true will return sensitive fields
 	 */
-	private async _find(
+	public async find(
 		filter:QueryFilter<Resource>,
 		projection?:QueryFilter<Resource> | string | null,
-		options?:QueryOptions | null,
-		with_sensitive:boolean
+		options?:QueryOptions<Resource>
 	):Promise<Resource[]>{
 		
+		// validate filter projection options
+		// -> this._relation.find()
+		// loop and create Resource[] w/w/o sensitive
+		
+		const urn_res_valid_fil_pro_opt = this._validate_filter_projection_options_params(
+			filter,
+			projection,
+			options
+		);
+		if(urn_res_valid_fil_pro_opt.error)
+			return urn_res_valid_fil_pro_opt as urn_mid.IURNFail;
+		return await urn_mid.async_res<ObjectType[]>(
+			async () => {
+				let mon_res_find = await this.relation.find(filter, projection, options);
+				if(!mon_res_find)
+					return mon_res_find;
+				return mon_res_find.map((mongoose_document:any) => {
+					return new this.OBJCLASS(mongoose_document).return(with_sensitive);
+				})
+			},'URNDAL.find'
+		)();
 		return await this._db_relation.find();
 	}
 	
-	public async find()
-			:Promise<Resource[]>{
-		return await this._find();
+	public async find_by_id()
+			:Promise<Resource>{
+		// TODO
 	}
 	
-	public async find_with_sensitive()
-			:Promise<Resource[]>{
-		return await this._find();
+	public async find_one()
+			:Promise<Resource>{
+		// TODO
 	}
 	
-	private async _find_by_id(){
-		// TODO implement
+	public async insert_one()
+			:Promise<Resource>{
+		// TODO
 	}
 	
-	public async find_by_id(){
-		return await this._find_by_id();
+	public async update_one()
+			:Promise<Resource>{
+		// TODO
 	}
 	
-	public async find_by_id_with_sensitive(){
-		return await this._find_by_id();
+	public async delete_one()
+			:Promise<Resource>{
+		// TODO
 	}
 	
-	private async _find_one(){
-		// TODO implement
-	}
+	private _validate_filter_projection_options_params(
+		filter:QueryFilter<Resource>,
+		projection?:QueryFilter<Resource> | string | null,
+		options?:QueryOptions<Resource>
+	):true | never{
 	
-	public async find_one(){
-		return await this._find_one();
-	}
-	
-	public async find_one_with_sensitive(){
-		return await this._find_one();
-	}
-	
-	private async _insert_one(){
-		// TODO implement
-	}
-	
-	public async insert_one(){
-		return await this._insert_one();
-	}
-	
-	public async insert_one_with_sensitive(){
-		return await this._insert_one();
-	}
-	
-	private async _update_one(){
-		// TODO implement
-	}
-	
-	public async update_one(){
-		return await this._update_one();
-	}
-	
-	public async update_one_with_sensitive(){
-		return await this._update_one();
-	}
-	
-	private async _delete_one(){
-		// TODO implement
-	}
-	
-	public async delete_one(){
-		return await this._delete_one();
-	}
-	
-	public async delete_one_with_sensitive(){
-		return await this._delete_one();
-	}
-	
-	public async is_valid_id(){
-		// TODO implement
 	}
 
+	private _validate_filter(filter:QueryFilter<Resource>)
+			:true | never{
+	
+	}
+	
+	private _validate_projection_options(
+		projection:QueryFilter<Resource>,
+		options:QueryOptions<Resource>
+	):true | never{
+	
+	}
+	
+	private _validate_projection(projection:QueryFilter<Resource>)
+			:true | never{
+	
+	}
+	
+	/**
+	 * Validate option object|string for querying Relation. Used in find, findOne, ...
+	 *
+	 * @param object - The object or the string to validate as option
+	 *
+	 */
+	private _validate_options(options:QueryOptions<Resource>)
+			:true | never{
+		if(urn_util.object.has_key(options, 'sort')){
+			switch(typeof options.sort){
+				case 'string':{
+					let sort_value = options.sort;
+					if(options.sort[0] == '+' || options.sort[0] == '-'){
+						sort_value = sort_value.substring(1, options.sort.length);
+					}
+					// if(sort_value != '_id' && !this.getApprovedFields().includes(sort_value)){
+					if(!this.getApprovedFields().includes(sort_value)){
+						throw urn_error.create(`Sort value not valid [${options.sort}]`);
+					}
+					break;
+				}
+				case 'object':{
+					for(const k in options.sort){
+						// if(k!='_id' && !this.getApprovedFields().includes(k))
+						if(!this.getApprovedFields().includes(k)){
+							throw urn_error.create(`Sort value not valid [${k}]`);
+						}
+						// if(!urn_util.object.has_key(options.sort, k)){
+						//   throw urn_error.create('Invalid key');
+						// }
+						const sort_obj_value = options.sort[k];
+						if(isNaN(sort_obj_value) || (sort_obj_value != -1 && sort_obj_value != 1)){
+							throw urn_error.create('Sort value must be equal either to -1 or 1');
+						}
+					}
+					break;
+				}
+			}
+		}
+		if(urn_util.object.has_key(options,'limit') && typeof options.limit != 'number'){
+			throw urn_error.create('Limit value type must be number');
+		}
+		if(urn_util.object.has_key(options,'skip') && typeof options.skip != 'number'){
+			throw urn_error.create('Skip value type must be number');
+		}
+		return true;
+	}
 }
 
 // export type DALInstance = InstanceType<typeof URNDAL>;
@@ -145,3 +188,4 @@ export abstract class URNDAL<Resource extends urn_rsrc.ResourceInstance> {
 //   return new URNDAL(name);
 	
 // }
+
