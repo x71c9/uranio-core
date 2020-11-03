85995ab5132509e8bc948eb4d7df94d3f99be7f5
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-11-03 15:24:10 +0100

    implemented dal query validation

--- src/dal/types.ts
+++ src/dal/types.ts
@@ -20,16 +20,60 @@ export interface QueryOptions<T> {
 	/*
 	 * Sort by field
 	 */
-	sort?: string|QueryFilter<T>;
+	sort?: string | QueryFilter<T>;
 	
 	/*
 	 * Limit records result
 	 */
-	limit?: number|null;
+	limit?: number | null;
 
 	/*
 	 * Skip number of records
 	 */
 	skip?: number;
 }
+
+
+type FilterAndOrNorType = {
+	
+	$and: any[],
+	
+	$or: any[],
+	
+	$now: any[],
+	
+	$not: any[]
+	
+};
+
+type FilterComparsionType = {
+	
+	$eq: string | number,
+	
+	$gt: string | number,
+	
+	$gte: string | number,
+	
+	$in: [string | number],
+	
+	$lt: string | number,
+	
+	$lte: string | number,
+	
+	$ne: string | number,
+	
+	$nin: [string | number]
+}
+
+type ConditionalQueryFilter = {
+	[P in keyof FilterAndOrNorType]: any[]
+}
+
+export type ComparsionQueryFilter = {
+	[P in keyof FilterComparsionType]: any
+}
+
+export type FilterType<T> = QueryFilter<T> | ConditionalQueryFilter;
+
+
 
