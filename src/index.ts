5819c0690762a4dde806a6de61b69bd4133d6cc5
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-11-10 18:32:00 +0100

    started update_one. not working yet.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@
 
 // import mongoose from 'mongoose';
 
-// import * as urn_atom from './atom/';
+import * as urn_atom from './atom/';
 
 // const mongoose_options = {
 //   useNewUrlParser: true,
@@ -41,27 +41,28 @@ import create_user_dal from './dal/users';
 
 // urn_log.defaults.log_level = urn_log.LogLevel.FUNCTION_DEBUG;
 
-// function makeid(length:number) {
-//   let result = '';
-//   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//   const charactersLength = characters.length;
-//   for ( let i = 0; i < length; i++ ) {
-//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//   }
-//   return result;
-// }
+function makeid(length:number) {
+	let result = '';
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	const charactersLength = characters.length;
+	for ( let i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
 
-// const user = {
-//   first_name: 'Federico',
-//   last_name: 'Reale',
-//   email: `a@a${makeid(9)}.com`,
-//   username: `sakjd${makeid(9)}las`,
-//   password: 'skajdlsadlSSKLJ@à2',
-//   active: true,
-//   bio: 'S',
-//   type: 'pro',
-//   creation_date: new Date()
-// };
+const user = {
+	_id:'5faa5acc1628f87526de20f4',
+	first_name: 'Federico',
+	last_name: 'Reale',
+	email: `a@a${makeid(9)}.com`,
+	username: `sakjd${makeid(9)}las`,
+	password: 'skajdlsadlSSKLJ@à2',
+	active: true,
+	bio: 'S',
+	type: 'pro',
+	creation_date: new Date()
+};
 
 // // const model = new Model(user);
 
@@ -107,12 +108,15 @@ const dal_users = create_user_dal();
 //   console.log('FIND', data);
 // });
 
-// dal_users.find_by_id('5faa5a6b20082873e570d450').then((data:any) => {
-//   console.log('FIND BY ID', data);
-// });
+dal_users.find_by_id('5faa5acc1628f87526de20f4').then((data:any) => {
+	console.log('FIND BY ID', data);
+});
 
-dal_users.find_one({first_name:'Federico'},{sort: '-creation_date'}).then((data:any) => {
+dal_users.find_one({_id:'5faa5acc1628f87526de20f4'},{sort: '-creation_date'}).then((data:any) => {
 	console.log('FIND ONE', data);
 });
 
+dal_users.update_one(urn_atom.user.module.create(user)).then((data:any) => {
+	console.log('UPDATE ONE', data);
+});
 
