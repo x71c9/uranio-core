f369b43e8c1924b895e4b06b9f41697aee96b80c
Andrea Reni <mail@nbl7.net>
2020-11-13 13:48:16 +0100

    dal abstract private function _find, _find_one, ... new trash functions, trash_find, trash_find_one, ...

--- src/dal/abstract.ts
+++ src/dal/abstract.ts
@@ -35,98 +35,144 @@ export abstract class DAL<M extends urn_atm.models.Resource, A extends urn_atm.A
 		this._db_trash_relation = new urn_rel.mongo.MongooseTrashRelation<M>(this._atom_module.relation_name);
 	}
 	
-	/**
-	 * Method that returns a colleciton of records from a Relation
-	 *
-	 * @param filter - Filter object for query
-	 *   e.g. {field0: 'value', field1: {$gt: 77}}
-	 * @param options [optional] - Option object
-	 *   e.g. {sort: 'field0', limit: 10, skip: 20}
-	 */
 	public async find(filter:QueryFilter<M>, options?:QueryOptions<M>)
 			:Promise<Array<A | null>>{
-		
+		return await this._find(filter, options);
+	}
+	
+	public async find_by_id(id:string)
+			:Promise<A | null>{
+		return await this._find_by_id(id);
+	}
+	
+	public async find_one(filter:QueryFilter<M>, options?:QueryOptions<M>)
+			:Promise<A | null>{
+		return await this._find_one(filter, options);
+	}
+	
+	public async insert_one(atom:A)
+			:Promise<A | null>{
+		return await this._insert_one(atom);
+	}
+	
+	public async update_one(atom:A)
+			:Promise<A | null>{
+		return await this._update_one(atom);
+	}
+	
+	public async delete_one(atom:A)
+			:Promise<A | null>{
+		const db_res_delete = await this._delete_one(atom);
+		if(db_res_delete && this._db_trash_relation){
+			const in_trash = true;
+			this._insert_one(db_res_delete, in_trash);
+		}
+		return db_res_delete;
+	}
+	
+	public async trash_find(filter:QueryFilter<M>, options?:QueryOptions<M>)
+			:Promise<Array<A | null>>{
+		return await this._find(filter, options, true);
+	}
+	
+	public async trash_find_by_id(id:string)
+			:Promise<A | null>{
+		return await this._find_by_id(id, true);
+	}
+	
+	public async trash_find_one(filter:QueryFilter<M>, options?:QueryOptions<M>)
+			:Promise<A | null>{
+		return await this._find_one(filter, options, true);
+	}
+	
+	public async trash_insert_one(atom:A)
+			:Promise<A | null>{
+		return await this._insert_one(atom, true);
+	}
+	
+	public async trash_update_one(atom:A)
+			:Promise<A | null>{
+		return await this._update_one(atom, true);
+	}
+	
+	public async trash_delete_one(atom:A)
+			:Promise<A | null>{
+		return await this._delete_one(atom, true);
+	}
+	
+	private async _find(filter:QueryFilter<M>, options?:QueryOptions<M>, in_trash = false)
+			:Promise<Array<A | null>>{
+		if(this._db_trash_relation === null){
+			return [];
+		}
 		urn_validators.query.validate_filter_options_params(this._atom_module.keys, filter, options);
-		
-		const db_res_find = await this._db_relation.find(filter, options);
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
+		const db_res_find = await _relation.find(filter, options);
 		const atom_array = db_res_find.reduce<Array<A | null>>((result, db_record) => {
-			result.push(this._create_atom(db_record, 'find'));
+			const func_name = '_find' + (in_trash === true) ? ' [TRASH]' : '';
+			result.push(this._create_atom(db_record, func_name));
 			return result;
 		}, []);
 		return atom_array;
-		
 	}
 	
-	/**
-	 * Method that returns a record from a Relation matching a given `id`
-	 *
-	 * @param filter - Filter object for query
-	 *   e.g. {field0: 'value', field1: {$gt: 77}}
-	 * @param options [optional] - Option object
-	 *   e.g. {sort: 'field0', limit: 10, skip: 20}
-	 */
-	public async find_by_id(id:string)
+	private async _find_by_id(id:string, in_trash = false)
 			:Promise<A | null>{
+		if(this._db_trash_relation === null){
+			return null;
+		}
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
 		if(!this._db_relation.is_valid_id(id)){
 			throw urn_error.create(`DAL.find_by_id(). Invalid id.`);
 		}
-		const db_res_find_by_id = await this._db_relation.find_by_id(id);
-		return this._create_atom(db_res_find_by_id, 'find_by_id');
+		const db_res_find_by_id = await _relation.find_by_id(id);
+		const func_name = '_find_by_id' + (in_trash === true) ? ' [TRASH]' : '';
+		return this._create_atom(db_res_find_by_id, func_name);
 	}
 	
-	/**
-	 * Method that returns the first record from a Relation matching
-	 * filter and options
-	 *
-	 * @param filter - Filter object for query
-	 *   e.g. {field0: 'value', field1: {$gt: 77}}
-	 * @param options [optional] - Option object
-	 *   e.g. {sort: 'field0', limit: 10, skip: 20}
-	 */
-	public async find_one(filter:QueryFilter<M>, options?:QueryOptions<M>)
+	private async _find_one(filter:QueryFilter<M>, options?:QueryOptions<M>, in_trash = false)
 			:Promise<A | null>{
-		
-		urn_validators.query.validate_filter_options_params(this._atom_module.keys, filter, options);
-		
-		const db_res_find_one = await this._db_relation.find_one(filter, options);
-		return this._create_atom(db_res_find_one, 'find_one');
-	}
-	
-	/**
-	 * Method that insert a record into a Relation
-	 *
-	 * @param atom - the Atom to insert
-	 */
-	public async insert_one(atom:A)
-			:Promise<A | null>{
-		const db_res_insert = await this._db_relation.insert_one(atom.return());
-		return this._create_atom(db_res_insert, 'insert_one');
-	}
-	
-	/**
-	 * Method that insert a record into a Relation
-	 *
-	 * @param atom - the Atom to update
-	 */
-	public async update_one(atom:A)
-			:Promise<A | null>{
-		const db_res_update = await this._db_relation.update_one(atom.return());
-		return this._create_atom(db_res_update, 'update_one');
-	}
-	
-	/**
-	 * Method that delete a record from a Relation
-	 *
-	 * @param atom - the Atom to update
-	 */
-	public async delete_one(atom:A)
-			:Promise<A | null>{
-		const db_res_delete = await this._db_relation.delete_one(atom.return());
-		if(db_res_delete && this._db_trash_relation){
-			const db_trash_res = await this._db_trash_relation.insert_one(db_res_delete);
-			console.log('DB TRASH', db_trash_res);
+		if(this._db_trash_relation === null){
+			return null;
 		}
-		return this._create_atom(db_res_delete, 'delete_one');
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
+		urn_validators.query.validate_filter_options_params(this._atom_module.keys, filter, options);
+		const db_res_find_one = await _relation.find_one(filter, options);
+		const func_name = '_find_one' + (in_trash === true) ? ' [TRASH]' : '';
+		return this._create_atom(db_res_find_one, func_name);
+	}
+
+	private async _insert_one(atom:A, in_trash = false)
+			:Promise<A | null>{
+		if(this._db_trash_relation === null){
+			return null;
+		}
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
+		const db_res_insert = await _relation.insert_one(atom.return());
+		const func_name = '_insert_one' + (in_trash === true) ? ' [TRASH]' : '';
+		return this._create_atom(db_res_insert, func_name);
+	}
+	
+	private async _update_one(atom:A, in_trash = false)
+			:Promise<A | null>{
+		if(this._db_trash_relation === null){
+			return null;
+		}
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
+		const db_res_insert = await _relation.update_one(atom.return());
+		const func_name = '_update_one' + (in_trash === true) ? ' [TRASH]' : '';
+		return this._create_atom(db_res_insert, func_name);
+	}
+	
+	private async _delete_one(atom:A, in_trash = false)
+			:Promise<A | null>{
+		if(this._db_trash_relation === null){
+			return null;
+		}
+		const _relation = (in_trash === true) ? this._db_relation : this._db_trash_relation;
+		const db_res_insert = await _relation.delete_one(atom.return());
+		const func_name = '_delete_one' + (in_trash === true) ? ' [TRASH]' : '';
+		return this._create_atom(db_res_insert, func_name);
 	}
 	
 	/**
@@ -135,12 +181,12 @@ export abstract class DAL<M extends urn_atm.models.Resource, A extends urn_atm.A
 	 * @param resource - The resource from a Relation method
 	 * @param func_name - DAL method name
 	 */
-	private _create_atom(resource:M | null, func_name:keyof DAL<M,A>)
+	private _create_atom(resource:M | null, func_name:string)
 			:A | null{
 		try{
 			return (!resource) ? null : this._atom_module.create(resource);
 		}catch(err){
-			let err_msg = `DAL.${func_name}(). Cannot create Atom.`;
+			let err_msg = `DAL.${func_name}. Cannot create Atom.`;
 			err_msg += ` DAL.relation_name [${this._atom_module.relation_name}].`;
 			if(resource && resource._id)
 				err_msg += ` Record _id [${resource._id}]`;
