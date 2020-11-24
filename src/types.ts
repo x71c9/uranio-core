996bb27155dcf319d43c2bf8248a055aae82c49c
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-11-24 12:42:12 +0100

    status: working. better name for query logic type.

--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,7 @@ export type DBType = 'mongo'; // | 'mysql'
 
 export type RelationName = 'urn_user'; // | 'urn_media';
 
+
 type KeysOfType<T> = {
 	
 	[P in keyof T]?:any;
@@ -24,30 +25,7 @@ export type QueryOptions<T> = {
 	
 }
 
-type FilterComparsionType = {
-	
-	$eq: string | number,
-	
-	$gt: string | number,
-	
-	$gte: string | number,
-	
-	$in: [string | number],
-	
-	$lt: string | number,
-	
-	$lte: string | number,
-	
-	$ne: string | number,
-	
-	$nin: [string | number]
-}
-
-export type ComparsionQueryFilter = {
-	[P in keyof FilterComparsionType]?: any
-}
-
-type FilterAndOrNorType<M> = {
+type FilterLogicType<M> = {
 	
 	$and?: KeysOfType<M>[],
 	
@@ -59,31 +37,32 @@ type FilterAndOrNorType<M> = {
 	
 };
 
-// type ConditionalQueryFilter<M> = {
-//   [P in keyof FilterAndOrNorType<M>]?: KeysOfType<M>[]
+export type FilterType<T> = KeysOfType<T> & FilterLogicType<T>;
+
+
+
+// type FilterComparsionType = {
+	
+//   $eq: string | number,
+	
+//   $gt: string | number,
+	
+//   $gte: string | number,
+	
+//   $in: [string | number],
+	
+//   $lt: string | number,
+	
+//   $lte: string | number,
+	
+//   $ne: string | number,
+	
+//   $nin: [string | number]
 // }
 
-// export type FilterType<T> = KeysOfType<T> | ConditionalQueryFilter<T>;
-export type FilterType<T> = KeysOfType<T> & FilterAndOrNorType<T>;
+// export type ComparsionQueryFilter = {
+//   [P in keyof FilterComparsionType]?: any
+// }
 
 
-
-export const _queryop = {
-	andornor: {
-		$and: null,
-		$or: null,
-		$nor: null,
-		$not: null
-	},
-	comparsion: {
-		$eq: null,
-		$gt: null,
-		$gte: null,
-		$in: null,
-		$lt: null,
-		$lte: null,
-		$ne: null,
-		$nin: null
-	}
-};
 
