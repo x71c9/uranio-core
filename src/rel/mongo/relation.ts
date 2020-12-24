4ffe168996cc331cc236d7449f0f10877028186f
Andrea Reni <mail@nbl7.net>
2020-12-24 09:46:59 +0100

    status: working. renamed variables and parameters.

--- src/rel/mongo/relation.ts
+++ src/rel/mongo/relation.ts
@@ -91,12 +91,12 @@ export class MongooseRelation<A extends AtomName> implements Relation<A> {
 		return _clean_object(mon_find_one_res);
 	}
 	
-	public async insert_one(resource:AtomShape<A>)
+	public async insert_one(atom_shape:AtomShape<A>)
 			:Promise<Atom<A>>{
-		if(urn_util.object.has_key(resource, '_id')){
-			delete (resource as any)._id;
+		if(urn_util.object.has_key(atom_shape, '_id')){
+			delete (atom_shape as any)._id;
 		}
-		const mon_model = new this._raw(resource);
+		const mon_model = new this._raw(atom_shape);
 		const mon_res_doc = await mon_model.save();
 		const str_id = mon_res_doc._id.toString();
 		const mon_obj = mon_res_doc.toObject();
@@ -104,19 +104,19 @@ export class MongooseRelation<A extends AtomName> implements Relation<A> {
 		return mon_obj;
 	}
 	
-	public async alter_one(resource:Atom<A>)
+	public async alter_one(atom:Atom<A>)
 			:Promise<Atom<A>>{
-		return await this.alter_by_id(resource._id, resource);
-		// if(!urn_util.object.has_key(resource, '_id')){
+		return await this.alter_by_id(atom._id, atom);
+		// if(!urn_util.object.has_key(atom, '_id')){
 		//   const err_msg = `Cannot alter_one. Argument has no _id.`;
 		//   throw urn_exc.create('ALTER_ONE_NO_ID', err_msg);
 		// }
-		// if(typeof resource._id !== 'string' || resource._id === '' || !this.is_valid_id(resource._id)){
+		// if(typeof atom._id !== 'string' || atom._id === '' || !this.is_valid_id(atom._id)){
 		//   const err_msg = `Cannot alter_one. Argument has invalid _id.`;
 		//   throw urn_exc.create('ALTER_ONE_INVALID_ID', err_msg);
 		// }
 		// const mon_update_res =
-		//   await this._raw.findOneAndUpdate({_id:resource._id}, resource, {new: true, lean: true});
+		//   await this._raw.findOneAndUpdate({_id:atom._id}, atom, {new: true, lean: true});
 		// if(mon_update_res === null){
 		//   throw urn_exc.create('ALTER_ONE_NOT_FOUND', `Cannot alter_one. Record not found.`);
 		// }
@@ -141,19 +141,19 @@ export class MongooseRelation<A extends AtomName> implements Relation<A> {
 		// return _clean_object(mon_obj);
 	}
 	
-	public async delete_one(resource:Atom<A>)
+	public async delete_one(atom:Atom<A>)
 			:Promise<Atom<A>>{
-		return await this.delete_by_id(resource._id);
-		// if(!urn_util.object.has_key(resource, '_id')){
+		return await this.delete_by_id(atom._id);
+		// if(!urn_util.object.has_key(atom, '_id')){
 		//   const err_msg = `Cannot delete_one. Argument has no _id.`;
 		//   throw urn_exc.create('DEL_ONE_NO_ID', err_msg);
 		// }
-		// if(typeof resource._id !== 'string' || resource._id === '' || !this.is_valid_id(resource._id)){
+		// if(typeof atom._id !== 'string' || atom._id === '' || !this.is_valid_id(atom._id)){
 		//   const err_msg = `Cannot delete_one. Argument has invalid _id.`;
 		//   throw urn_exc.create('DEL_ONE_INVALID_ID', err_msg);
 		// }
 		// const mon_delete_res =
-		//   await this._raw.findOneAndDelete({_id:resource._id});
+		//   await this._raw.findOneAndDelete({_id:atom._id});
 		// if(typeof mon_delete_res !== 'object' ||  mon_delete_res === null){
 		//   throw urn_exc.create_not_found('DEL_ONE_NOT_FOUND', `Cannot delete_one. Record not found.`);
 		// }
@@ -181,15 +181,15 @@ export class MongooseRelation<A extends AtomName> implements Relation<A> {
 	
 }
 
-function _clean_object<A extends AtomName>(resource:Atom<A>)
+function _clean_object<A extends AtomName>(atom:Atom<A>)
 		:Atom<A>{
-	if(urn_util.object.has_key(resource,'_id')){
-		resource._id = resource._id.toString();
+	if(urn_util.object.has_key(atom,'_id')){
+		atom._id = atom._id.toString();
 	}
-	if(urn_util.object.has_key(resource,'__v')){
-		delete (resource as any).__v;
+	if(urn_util.object.has_key(atom,'__v')){
+		delete (atom as any).__v;
 	}
-	return resource;
+	return atom;
 }
 
 export function create<A extends AtomName>(atom_name: A)
