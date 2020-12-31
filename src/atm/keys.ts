ba18f273887467fffe8c9e81dfa21a9bbfbdb259
Andrea Reni <mail@nbl7.net>
2020-12-31 17:15:13 +0100

    status: running. fixed mongo relation clean molecule for subatom arrays.

--- src/atm/keys.ts
+++ src/atm/keys.ts
@@ -11,6 +11,7 @@ import {
 	AtomName,
 	Atom,
 	AtomShape,
+	Molecule,
 	Book,
 	BookPropertyType
 } from '../types';
@@ -49,14 +50,40 @@ export function get_unique_keys<A extends AtomName>(atom_name:A)
 }
 
 export function get_subatom_keys<A extends AtomName>(atom_name:A)
-		:Set<keyof AtomShape<A>>{
-	const subatom_keys = new Set<keyof AtomShape<A>>();
+		:Set<keyof Molecule<A>>{
+	const subatom_keys = new Set<keyof Molecule<A>>();
 	const atom_props = atom_book[atom_name]['properties'] as Book.Definition.Properties;
 	for(const k in atom_props){
 		const prop:Book.Definition.Property = atom_props[k];
 		if(prop.type && prop.type === BookPropertyType.ATOM || prop.type === BookPropertyType.ATOM_ARRAY){
-			subatom_keys.add(k as keyof AtomShape<A>);
+			subatom_keys.add(k as keyof Molecule<A>);
 		}
 	}
 	return subatom_keys;
 }
+
+// export function get_subatom_array_keys<A extends AtomName>(atom_name:A)
+//     :Set<keyof AtomShape<A>>{
+//   const subatom_keys = new Set<keyof AtomShape<A>>();
+//   const atom_props = atom_book[atom_name]['properties'] as Book.Definition.Properties;
+//   for(const k in atom_props){
+//     const prop:Book.Definition.Property = atom_props[k];
+//     if(prop.type === BookPropertyType.ATOM_ARRAY){
+//       subatom_keys.add(k as keyof AtomShape<A>);
+//     }
+//   }
+//   return subatom_keys;
+// }
+
+// export function get_subatom_non_array_keys<A extends AtomName>(atom_name:A)
+//     :Set<keyof AtomShape<A>>{
+//   const subatom_keys = new Set<keyof AtomShape<A>>();
+//   const atom_props = atom_book[atom_name]['properties'] as Book.Definition.Properties;
+//   for(const k in atom_props){
+//     const prop:Book.Definition.Property = atom_props[k];
+//     if(prop.type === BookPropertyType.ATOM){
+//       subatom_keys.add(k as keyof AtomShape<A>);
+//     }
+//   }
+//   return subatom_keys;
+// }
