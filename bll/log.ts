0d9489ff7c328ff5cee934abafbbb40d8cf33d66
Andrea Reni <mail@nbl7.net>
2021-02-08 20:16:05 +0100

    Implemented LogName.

--- bll/log.ts
+++ bll/log.ts
@@ -6,16 +6,18 @@
 
 import {urn_log} from 'urn-lib';
 
+import {LogName} from '../types';
+
 import {BasicBLL} from './basic';
 
 @urn_log.decorators.debug_constructor
 @urn_log.decorators.debug_methods
-export class LogBLL extends BasicBLL<'log'>{}
+export class LogBLL<A extends LogName> extends BasicBLL<A>{}
 
-export function create_log()
-		:LogBLL{
-	urn_log.fn_debug(`Create LogBLL`);
-	return new BasicBLL<'log'>('log');
+export function create_log<A extends LogName>(log_name:A)
+		:LogBLL<A>{
+	urn_log.fn_debug(`Create LogBLL [${log_name}]`);
+	return new BasicBLL<A>(log_name);
 }
 
 
--- dal/log.ts
+++ dal/log.ts
@@ -6,19 +6,21 @@
 
 import {urn_log} from 'urn-lib';
 
+import {LogName} from '../types';
+
 import {DAL} from './dal';
 
 @urn_log.decorators.debug_constructor
 @urn_log.decorators.debug_methods
-export class LogDAL extends DAL<'log'>{
-	constructor(){
-		super('log');
+export class LogDAL<A extends LogName> extends DAL<A>{
+	constructor(log_name:A){
+		super(log_name);
 	}
 }
 
-export function create_log()
-		:LogDAL{
-	urn_log.fn_debug(`Create LogDAL`);
-	return new LogDAL();
+export function create_log<A extends LogName>(log_name:A)
+		:LogDAL<A>{
+	urn_log.fn_debug(`Create LogDAL [${log_name}]`);
+	return new LogDAL(log_name);
 }
 
