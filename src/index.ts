2033f1db0f8b787f0cb990f5990ab59a8c751b98
Andrea Reni <mail@nbl7.net>
2021-04-04 18:30:24 +0200

    removed everything but repo.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-
-import {urn_log} from 'urn-lib';
-
-import uranio from 'uranio';
-
-urn_log.defaults.log_level = urn_log.LogLevel.FUNCTION_DEBUG;
-
-const su = uranio.bll.basic.create('superuser');
-su.find({}).then(d => console.log(d));
