116dc7ca8d965e4cb3768b3001f6da7e63b4a9cf
Andrea Reni <mail@nbl7.net>
2021-07-22 15:58:14 +0200

    converted const enum to enum.

--- typ/auth.ts
+++ typ/auth.ts
@@ -20,7 +20,15 @@ export type Passport = {
 	[k in PassportKey]: MapType<typeof abstract_passport[k]>
 }
 
-export const enum AuthAction {
+// With "const" typescript will no transpile any code and delete the reference.
+// While without "const" ts will tranpile in a JS object. So it is possible to
+// check if a value is included in the enum.
+// https://thisthat.dev/const-enum-vs-enum/
+// It is possible to preserve the const with "preserveConstEnums" in tsconfig
+//
+// export const enum AuthAction {
+//
+export enum AuthAction {
 	READ = 'READ',
 	WRITE = 'WRITE',
 	AUTH = 'AUTH'
