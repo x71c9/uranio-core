b744ab21b56d09ee86468784167eb9b77a917dad
x71c9 <108585118+x71c9@users.noreply.github.com>
2021-07-26 20:45:49 +0200

    nst folder for instancing.

---
+++ nst/index.ts
@@ -0,0 +1,18 @@
+/**
+ * API Instances index module
+ *
+ * @packageDocumentation
+ */
+
+import * as bll from '../bll/';
+
+type ApiInstance<T> = T | undefined;
+
+let core_bll_group:ApiInstance<bll.BLL<'group'>>;
+
+export function get_bll_group():bll.BLL<'group'>{
+	if(!core_bll_group){
+		core_bll_group = bll.basic.create('group');
+	}
+	return core_bll_group;
+}
