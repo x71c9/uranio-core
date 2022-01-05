687b3961ba46c83a110d472a39303406d98a70e3
Andrea Reni <mail@nbl7.net>
2022-01-05 18:22:13 +0100

    re-import namespace in typ book.

--- typ/book_srv.ts
+++ typ/book_srv.ts
@@ -58,13 +58,53 @@ export namespace Book {
 		}
 		
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		// export import Dock = book_cln.Book.Definition.Dock;
+		// export import Properties = book_cln.Book.Definition.Properties;
+		
+		export type Properties = book_cln.Book.Definition.Properties;
 		
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		export import Properties = book_cln.Book.Definition.Properties;
+		// export import Property = book_cln.Book.Definition.Property;
 		
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		export import Property = book_cln.Book.Definition.Property;
+		export namespace Property {
+			
+			export type SharedFields = book_cln.Book.Definition.Property.SharedFields;
+			
+			export type ID = book_cln.Book.Definition.Property.ID;
+			export type Text = book_cln.Book.Definition.Property.Text;
+			export type LongText = book_cln.Book.Definition.Property.LongText;
+			export type String = book_cln.Book.Definition.Property.String;
+			export type Number = book_cln.Book.Definition.Property.Number;
+			export type Enum = book_cln.Book.Definition.Property.Enum;
+			export type Set = book_cln.Book.Definition.Property.Set;
+			export type DayTime = book_cln.Book.Definition.Property.DayTime;
+			export type Email = book_cln.Book.Definition.Property.Email;
+			export type Integer = book_cln.Book.Definition.Property.Integer;
+			export type Float = book_cln.Book.Definition.Property.Float;
+			export type Binary = book_cln.Book.Definition.Property.Binary;
+			export type Encrypted = book_cln.Book.Definition.Property.Encrypted;
+			export type Day = book_cln.Book.Definition.Property.Day;
+			export type Time = book_cln.Book.Definition.Property.Time;
+			export type EnumString = book_cln.Book.Definition.Property.EnumString;
+			export type EnumNumber = book_cln.Book.Definition.Property.EnumNumber;
+			export type SetString = book_cln.Book.Definition.Property.SetString;
+			export type SetNumber = book_cln.Book.Definition.Property.SetNumber;
+			export type Atom = book_cln.Book.Definition.Property.Atom;
+			export type AtomArray = book_cln.Book.Definition.Property.AtomArray;
+			
+			export namespace Format {
+				export type Float = book_cln.Book.Definition.Property.Format.Float;
+			}
+			
+			export namespace Validation {
+				export type String = book_cln.Book.Definition.Property.Validation.String;
+				export type Number = book_cln.Book.Definition.Property.Validation.Number;
+				export type DayTime = book_cln.Book.Definition.Property.Validation.DayTime;
+				export type SetNumber = book_cln.Book.Definition.Property.Validation.SetNumber;
+				export type SetString = book_cln.Book.Definition.Property.Validation.SetString;
+				export type Atom = book_cln.Book.Definition.Property.Validation.Atom;
+			}
+			
+		}
 		
 		export type Security = {
 			type: BookSecurityType,
