b6cbad2fd3fe806df2c9eb08b7e2090545ce6862
Andrea Reni <mail@nbl7.net>
2022-01-22 12:52:59 +0100

    updated presigned method.

--- sto/types.ts
+++ sto/types.ts
@@ -13,6 +13,9 @@ export interface Storage {
 	upload(filename:string, buffer:Buffer | ArrayBuffer | Blob, params?:Partial<UploadParams>)
 		:Promise<string>;
 	
+	presigned(filename:string, params?:Partial<UploadParams>)
+		:Promise<string>;
+	
 	exists(filename:string):Promise<boolean>
 	
 	base_url:string
