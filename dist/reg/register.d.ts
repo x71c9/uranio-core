33da67ad4b03181d7fd9988c87c15688ab468642
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-02-07 10:48:14 +0100

    added atom_name param.

--- dist/reg/register.d.ts
+++ dist/reg/register.d.ts
@@ -4,4 +4,4 @@
  * @packageDocumentation
  */
 import * as types from '../types';
-export declare function register(atom_definition: types.Book.Definition): types.Book.Definition;
+export declare function register(atom_name: 'string', atom_definition: types.Book.Definition): types.Book.Definition;
