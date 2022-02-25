c70ecab254e350512fa173bf63410c5af9b735bc
Andrea Reni <mail@nbl7.net>
2022-02-25 13:56:50 +0100

    upated register client server.

--- dist/reg/server.d.ts
+++ dist/reg/server.d.ts
@@ -5,4 +5,4 @@
  */
 import * as types from '../server/types';
 import { schema } from '../sch/server';
-export declare function register<A extends schema.AtomName>(atom_definition: types.Book.Definition<A>, atom_name?: string): string;
+export declare function register<A extends schema.AtomName>(atom_definition: types.Book.Definition<A>, atom_name?: A): string;
--- src/reg/server.ts
+++ src/reg/server.ts
@@ -4,32 +4,24 @@
  * @packageDocumentation
  */
 
-import path from 'path';
+// import path from 'path';
 
-import caller from 'caller';
+// import caller from 'caller';
 
-import {urn_log} from 'urn-lib';
+// import {urn_log} from 'urn-lib';
 
-import * as book from '../book/server';
+// import * as book from '../book/server';
 
 import * as types from '../server/types';
 
 import {schema} from '../sch/server';
 
-export function register<A extends schema.AtomName>(atom_definition:types.Book.Definition<A>, atom_name?:string)
-		:string{
-	let final_atom_name = `undefined_atom`;
-	if(atom_name){
-		final_atom_name = atom_name;
-	}else{
-		const caller_path = caller();
-		const dirname = path.dirname(caller_path);
-		final_atom_name =
-			dirname.split('/').slice(-1)[0].replace('.','_').replace('-','_');
-	}
-	urn_log.debug(`Registering atom [${final_atom_name}]...`);
-	book.add_definition(final_atom_name as A, atom_definition);
-	urn_log.debug(`Atom [${final_atom_name}] registered.`);
-	return final_atom_name;
+import {register as client_register} from './client';
+
+export function register<A extends schema.AtomName>(
+	atom_definition:types.Book.Definition<A>,
+	atom_name?:A
+):string{
+	return client_register(atom_definition, atom_name);
 }
 
