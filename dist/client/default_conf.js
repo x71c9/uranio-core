5d577a23c4d77f0ce1faeb4702b3663ff7e4e9ef
Andrea Reni <mail@nbl7.net>
2022-03-11 13:09:22 +0100

    compiled.

--- dist/client/default_conf.js
+++ dist/client/default_conf.js
@@ -13,11 +13,11 @@ exports.core_client_config = {
     log_max_str_length: 174,
     log_prefix: '',
     log_prefix_type: false,
-    log_dev_debug_info: true,
-    log_dev_color: true,
-    log_dev_time_format: 'yyyy-mm-dd\'T\'HH:MM:ss:l',
-    log_dev_max_str_length: 174,
-    log_dev_prefix: '',
-    log_dev_prefix_type: false,
+    dev_log_debug_info: true,
+    dev_log_color: true,
+    dev_log_time_format: 'yyyy-mm-dd\'T\'HH:MM:ss:l',
+    dev_log_max_str_length: 174,
+    dev_log_prefix: '',
+    dev_log_prefix_type: false,
 };
 //# sourceMappingURL=default_conf.js.map
