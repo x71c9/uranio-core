8a81c516aec4f3321f59159a9a4acb416e6dc287
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-05-13 13:19:05 +0200

    clean comments dev.

--- dist/dev.js
+++ dist/dev.js
@@ -10,18 +10,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
 server_1.default.init();
-// const bll = uranio.bll.basic.create('superuser');
-// bll.search('andr').then((a) => {
-// 	console.log(a);
-// });
-// const bll = uranio.bll.basic.create('error');
-// bll.find_by_id('625bddfde2f65c4d52a57892', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
-// bll.find_by_id('61e9980a5bea5d4f9044f19b', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
-// bll.update_by_id('61e9980a5bea5d4f9044f19b', {email: 'v@a.com'}, {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
 //# sourceMappingURL=dev.js.map
--- src/dev.ts
+++ src/dev.ts
@@ -6,20 +6,3 @@
 
 import uranio from './server';
 uranio.init();
-
-// const bll = uranio.bll.basic.create('superuser');
-// bll.search('andr').then((a) => {
-// 	console.log(a);
-// });
-
-// const bll = uranio.bll.basic.create('error');
-// bll.find_by_id('625bddfde2f65c4d52a57892', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
-
-// bll.find_by_id('61e9980a5bea5d4f9044f19b', {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
-// bll.update_by_id('61e9980a5bea5d4f9044f19b', {email: 'v@a.com'}, {depth: 1}).then((a) => {
-// 	console.log(a);
-// });
