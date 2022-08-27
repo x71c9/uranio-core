c6dfbf27d158e04fc52623250f4b3a3459e5bc0d
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-08-27 11:36:51 +0200

    cleaned dist folder.

--- dist/atm/prefix.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Module for defining prefix for default atoms
- *
- * @packageDocumentation
- */
-declare const _default: "_";
-export default _default;
--- dist/atm/prefix.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Module for defining prefix for default atoms
- *
- * @packageDocumentation
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = '_';
-//# sourceMappingURL=prefix.js.map
