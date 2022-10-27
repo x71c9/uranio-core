fbc0d49ced33ecfede4f620d834a2c57b2bb5d90
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-10-27 11:20:31 +0200

    added and fixed path from service/ws.

--- dist/service/ws.d.ts
+++ dist/service/ws.d.ts
@@ -1,21 +1,8 @@
 #!/usr/bin/env node
-
 /**
  * Core binary that will initialized uranio and import delta modules
  *
  * @packageDocumentation
  */
-
-import dotenv from 'dotenv';
-const result = dotenv.config();
-
-if(result.error){
-	throw result.error;
-}
-
 export * from '../srv/register';
-
-import uranio from '../server';
-uranio.init();
-
 export * from '../srv/delta/index';
---
+++ dist/service/ws.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+"use strict";
+/**
+ * Core binary that will initialized uranio and import delta modules
+ *
+ * @packageDocumentation
+ */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const result = dotenv_1.default.config();
+if (result.error) {
+    throw result.error;
+}
+__exportStar(require("../srv/register"), exports);
+const server_1 = __importDefault(require("../server"));
+server_1.default.init();
+__exportStar(require("../srv/delta/index"), exports);
+//# sourceMappingURL=ws.js.map
